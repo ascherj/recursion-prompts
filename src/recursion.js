69cbcdfdc1f20522fd3328b4d1fe8de4292d284e
Jake Ascher <jake.ascher@galvanize.com>
2021-04-28 11:25:47 -0400

    Complete palindrome()

--- src/recursion.js
+++ src/recursion.js
@@ -125,6 +125,15 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  var formattedString = string.replace(' ', '').toLowerCase();
+
+  if (formattedString.length <= 1) {
+    return true;
+  }
+  if (formattedString[0] !== formattedString[formattedString.length - 1]) {
+    return false;
+  }
+  return palindrome(formattedString.slice(1, -1));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
