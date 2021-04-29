c8eee483d9d9728af9554109abe40047958f2438
Jake Ascher <jake.ascher@galvanize.com>
2021-04-29 17:15:28 -0400

    Complete compareStr()

--- src/recursion.js
+++ src/recursion.js
@@ -221,6 +221,13 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (!str1 && !str2) {
+    return true;
+  }
+  if (str1[0] !== str2[0]) {
+    return false;
+  }
+  return compareStr(str1.slice(1), str2.slice(1));
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
